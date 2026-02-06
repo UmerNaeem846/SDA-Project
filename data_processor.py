5db16c7200b92ee7f6b4b279d754a8bea335abcb
UmerNaeem846 <73685099+UmerNaeem846@users.noreply.github.com>
2026-02-06 20:17:34 +0500

    Data Load and Process

---
+++ data_processor.py
@@ -0,0 +1,29 @@
+def data_conversion(data):
+    year_col=list(filter(lambda y: y.isdigit(), data[0].keys()))
+    
+    return[
+        {
+            "Country":row["Country Name"],
+            "Region":row["Continent"],
+            "Year":int(year),
+            "Values": float(row[year])
+            if row[year]
+            else None
+        }
+        
+        for row in data
+        for year in year_col
+        if row[year]!=""
+    ]
+
+def clean_data(data):
+    return list(filter(lambda col: col["Value"] is not None, data))
+
+def region_filter(data, region):
+    return list(filter(lambda col:col["Region"]==region,data ))
+
+def year_filter(data, year):
+    return list(filter(lambda col:col["Year"]==year,data ))
+
+def country_filter(data, country):
+    return list(filter(lambda col:col["Country"]==country,data ))
