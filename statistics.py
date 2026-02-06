90b67ad83ca6d4eb4481ae8814f31447396a5f32
UmerNaeem846 <73685099+UmerNaeem846@users.noreply.github.com>
2026-02-06 20:19:17 +0500

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,1 +1,6 @@
-def 
+def average_gdp_region(data):
+    values = list(map(lambda col: col["Value"], data))
+    return sum(values) / len(values) if values else 0
+
+def sum_gdp_region(data):
+    return sum(map(lambda col: col["Value"], data))
